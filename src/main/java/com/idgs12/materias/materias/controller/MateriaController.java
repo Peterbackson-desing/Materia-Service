e577418560be27064bbc41e92694e3f006450177
Peterbackson-design <2023171040@uteq.edu.mx>
2025-11-14 23:33:46 -0600

    Agrega funcionalidad de editar materia

--- src/main/java/com/idgs12/materias/materias/controller/MateriaController.java
+++ src/main/java/com/idgs12/materias/materias/controller/MateriaController.java
@@ -1,12 +1,50 @@
 package com.idgs12.materias.materias.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.idgs12.materias.materias.dto.MateriaDTO;
+import com.idgs12.materias.materias.services.MateriaService;
 
 @RestController
 @RequestMapping("/materias")
 @CrossOrigin(origins = "*")
 public class MateriaController {
+
+    @Autowired
+    private MateriaService materiaService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<MateriaDTO>> findAll() {
+        List<MateriaDTO> materias = materiaService.findAll();
+        return ResponseEntity.ok(materias);
+    }
+    //Funcionalidad de habilitar --Maria Fernanda Rosas Briones- idgs12
+    @PutMapping("/habilitar/{id}")
+    public ResponseEntity<String> habilitarMateria(@PathVariable int id) {
+        boolean habilitada = materiaService.habilitarMateria(id);
+
+        if (habilitada) {
+            return ResponseEntity.ok("Materia habilitada correctamente");
+        } else {
+            return ResponseEntity.badRequest().body("No se pudo habilitar la materia");
+        }
+    }
+
+    //funcion de editar- pedrito bb
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<?> editarMateria(@PathVariable int id, @RequestBody MateriaDTO materiaDTO) {
+
+        MateriaDTO materiaEditada = materiaService.editarMateria(id, materiaDTO);
+
+        if (materiaEditada == null) {
+            return ResponseEntity.badRequest().body("No se pudo actualizar la materia");
+        }
+
+        return ResponseEntity.ok(materiaEditada);
+    }
 
 }
